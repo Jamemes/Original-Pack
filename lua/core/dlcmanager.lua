05819c40223175010d9000879d0cf8c1e4899ab4
Jamemes <coderjames97@gmail.com>
2024-01-31 20:41:26 +0300

    1.4.1.5.1

--- lua/core/dlcmanager.lua
+++ lua/core/dlcmanager.lua
@@ -1,13 +1,11 @@
+local data = GenericDLCManager.give_dlc_and_verify_blackmarket
 function GenericDLCManager:give_dlc_and_verify_blackmarket()
-	self:give_dlc_package()
+	data(self)
+
 	if managers.blackmarket then
-		managers.blackmarket:tradable_dlcs()
-		managers.blackmarket:verify_dlc_items()
 		if not Global.blackmarket_manager.max_progress and _G.OriginalPackOptions.settings.Enable_Max_Progress then
 			Global.blackmarket_manager.max_progress = true
 			managers.blackmarket:max_progress()
 		end
-	else
-		Application:error("[GenericDLCManager] _load_done(): BlackMarketManager not yet initialized!")
 	end
 end
